92a35f0ba0710fed02cda7f0b138dcd167c129ba
yxche-scholar <yxche@outlook.com>
2026-02-11 16:12:45 +0800

    remove old files

--- src/lib/utils.ts
+++
@@ -1,8 +0,0 @@
-export function formatDate(date: Date){
-    return new Intl.DateTimeFormat("en-US", {
-        weekday: "long",
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-      }).format(date)
-}
